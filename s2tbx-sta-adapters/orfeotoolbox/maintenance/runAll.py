33d5947607f00fbb28b48d14e66569bfc0126820
Julien Malik <julien.malik@paraiso.me>
2016-03-23 17:24:28 +0100

    Remove arguments from OTB adapters generation script

--- s2tbx-sta-adapters/orfeotoolbox/maintenance/runAll.py
+++ s2tbx-sta-adapters/orfeotoolbox/maintenance/runAll.py
@@ -5,18 +5,10 @@ import glob
 import os
 from ToolAdapterGenerator import run_ToolAdapterGenerator
 
-
-def getArguments():
-    outputDirectory = sys.argv[1]
-    if not os.path.isdir(outputDirectory):
-        os.mkdir(outputDirectory)
-    print sys.argv[3], bool(int(sys.argv[3]))
-    return outputDirectory, os.path.realpath(sys.argv[2]), bool(int(sys.argv[3]))
-
-
 if __name__ == '__main__':
-    outputDirectory, directoryContainingProcessingXML, createAdapter = getArguments()
-    print createAdapter
+    outputDirectory = os.path.join(os.path.dirname(__file__), '..')
+    directoryContainingProcessingXML = os.path.join(os.path.dirname(__file__), 'xml')
+    createAdapter = True
     listXmlFiles = glob.glob(os.path.join(directoryContainingProcessingXML, "*.xml"))
     print listXmlFiles
     for xmlProcessing in listXmlFiles:

12aa78578a9ee829598f0b0c6a6b0392d0e041d1
Olaf Danne <olaf.danne@brockmann-consult.de>
2016-04-22 14:42:32 +0200

    initial checkin of Sentinel-2 Idepix (branch s2_idepix)

---
+++ s2tbx-idepix-ui/src/main/java/org/esa/s2tbx/idepix/docs/package-info.java
@@ -0,0 +1,4 @@
+@HelpSetRegistration(helpSet = "help.hs", position = 4020)
+package org.esa.s2tbx.idepix.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
---
+++ s2tbx-idepix/src/main/java/org/esa/s2tbx/idepix/algorithms/sentinel2/Sentinel2Algorithm.java
@@ -0,0 +1,241 @@
+package org.esa.s2tbx.idepix.algorithms.sentinel2;
+
+import org.esa.s2tbx.idepix.util.IdepixConstants;
+import org.esa.s2tbx.idepix.util.IdepixUtils;
+import org.esa.snap.core.util.math.MathUtils;
+
+/**
+ * todo: add comment
+ * To change this template use File | Settings | File Templates.
+ * Date: 11.04.2016
+ * Time: 13:38
+ *
+ * @author olafd
+ */
+public class Sentinel2Algorithm {
+
+    static final float UNCERTAINTY_VALUE = 0.5f;
+    static final float LAND_THRESH = 0.9f;
+    static final float WATER_THRESH = 0.9f;
+
+    static final float BRIGHTWHITE_THRESH = 1.5f;
+    static final float NDSI_THRESH = 0.68f;
+    static final float CLOUD_THRESH = 1.65f;
+    static final float BRIGHT_THRESH = 0.25f;
+    static final float BRIGHT_FOR_WHITE_THRESH = 0.8f;
+    static final float WHITE_THRESH = 0.9f;
+    static final float NDVI_THRESH = 0.5f;
+    static final float PRESSURE_THRESH = 0.9f;
+
+    private float[] refl;
+    private double brr442Thresh;
+    private double[] nnOutput;
+    private boolean isWater;
+
+
+    public boolean isBrightWhite() {
+        return !isInvalid() && (whiteValue() + brightValue() > getBrightWhiteThreshold());
+    }
+
+    public boolean isCloud() {
+        boolean threshTest = whiteValue() + brightValue() + pressureValue() + temperatureValue() > CLOUD_THRESH;
+        return !isInvalid() && (threshTest && !isClearSnow());
+    }
+
+    public boolean isClearLand() {
+        if (isInvalid()) {
+            return false;
+        }
+        float landValue;
+
+        if (!MathUtils.equalValues(radiometricLandValue(), UNCERTAINTY_VALUE)) {
+            landValue = radiometricLandValue();
+        } else if (aPrioriLandValue() > UNCERTAINTY_VALUE) {
+            landValue = aPrioriLandValue();
+        } else {
+            return false; // this means: if we have no information about land, we return isClearLand = false
+        }
+        return (!isWater && !isCloud() && landValue > LAND_THRESH);
+    }
+
+    public boolean isClearWater() {
+        if (isInvalid()) {
+            return false;
+        }
+        float waterValue;
+        if (!MathUtils.equalValues(radiometricWaterValue(), UNCERTAINTY_VALUE)) {
+            waterValue = radiometricWaterValue();
+        } else if (aPrioriWaterValue() > UNCERTAINTY_VALUE) {
+            waterValue = aPrioriWaterValue();
+        } else {
+            return false; // this means: if we have no information about water, we return isClearWater = false
+        }
+        return (isWater && !isCloud() && waterValue > WATER_THRESH);
+    }
+
+    public boolean isClearSnow() {
+        return (!isInvalid() && isLand() && isBrightWhite() && ndsiValue() > getNdsiThreshold());
+    }
+
+    public boolean isSeaIce() {
+        return false;
+    }
+
+    public boolean isLand() {
+        return aPrioriLandValue() > LAND_THRESH;
+    }
+
+    public boolean isWater() {
+        return !isInvalid() && !isLand() && isWater;
+    }
+
+    public boolean isBright() {
+        return brightValue() > getBrightThreshold();
+    }
+
+    public boolean isWhite() {
+        return whiteValue() > getWhiteThreshold();
+    }
+
+    public boolean isVegRisk() {
+        return ndviValue() > getNdviThreshold();
+    }
+
+    public boolean isHigh() {
+        return false;
+    }
+
+    public boolean isInvalid() {
+        return false;
+    }
+
+    // feature values
+    public float spectralFlatnessValue() {
+        final double slope0 = IdepixUtils.spectralSlope(refl[1], refl[0],
+                                                        IdepixConstants.S2_MSI_WAVELENGTHS[1],
+                                                        IdepixConstants.S2_MSI_WAVELENGTHS[0]);
+        final double slope1 = IdepixUtils.spectralSlope(refl[2], refl[3],
+                                                        IdepixConstants.S2_MSI_WAVELENGTHS[2],
+                                                        IdepixConstants.S2_MSI_WAVELENGTHS[3]);
+        final double slope2 = IdepixUtils.spectralSlope(refl[4], refl[6],
+                                                        IdepixConstants.S2_MSI_WAVELENGTHS[4],
+                                                        IdepixConstants.S2_MSI_WAVELENGTHS[6]);
+
+        final double flatness = 1.0f - Math.abs(1000.0 * (slope0 + slope1 + slope2) / 3.0);
+        // todo: check if it should be like this:
+//        final double flatness = 1.0f - Math.abs((slope0 + slope1 + slope2) / (3.0*1000.0));
+        return (float) Math.max(0.0f, flatness);
+    }
+
+    public float whiteValue() {
+        if (brightValue() > BRIGHT_FOR_WHITE_THRESH) {
+            return spectralFlatnessValue();
+        } else {
+            return 0.0f;
+        }
+    }
+
+    public float brightValue() {
+        if (refl[0] <= 0.0 || brr442Thresh <= 0.0) {
+            return IdepixConstants.NO_DATA_VALUE;
+        } else {
+            return (float) (refl[0] / (6.0 * brr442Thresh));
+        }
+    }
+
+    public float ndsiValue() {
+        double value = (refl[2] - refl[10]) / (refl[2] + refl[10]);
+        value = 20.0 * (value + 0.02);
+        value = Math.min(value, 1.0);
+        value = Math.max(value, 0.0);
+        return (float) value;
+    }
+
+    public float ndviValue() {
+        double value = (refl[5] - refl[3]) / (refl[5] + refl[3]);
+        value = 0.5 * (value + 1);
+        value = Math.min(value, 1.0);
+        value = Math.max(value, 0.0);
+        return (float) value;
+    }
+
+    public float pressureValue() {
+        return UNCERTAINTY_VALUE;
+    }
+
+    public float aPrioriLandValue() {
+        return UNCERTAINTY_VALUE;
+    }
+
+    public float aPrioriWaterValue() {
+        return UNCERTAINTY_VALUE;
+    }
+
+
+    // SETTERS
+    public void setRhoToa442Thresh(double brr442Thresh) {
+        this.brr442Thresh = brr442Thresh;
+    }
+
+    public void setRefl(float[] refl) {
+        this.refl = refl;
+    }
+
+    public void setNnOutput(double[] nnOutput) {
+        this.nnOutput = nnOutput;
+    }
+
+    public void setIsWater(boolean isWater) {
+        this.isWater = !isInvalid() && isWater;
+    }
+
+    // GETTERS
+    public float getNdsiThreshold() {
+        return NDSI_THRESH;
+    }
+
+    public float getNdviThreshold() {
+        return NDVI_THRESH;
+    }
+
+    public float getBrightThreshold() {
+        return BRIGHT_THRESH;
+    }
+
+    public float getWhiteThreshold() {
+        return WHITE_THRESH;
+    }
+
+    public double[] getNnOutput() {
+        return nnOutput;
+    }
+
+    public float temperatureValue() {
+        return UNCERTAINTY_VALUE;
+    }
+
+    public float radiometricLandValue() {
+        if (refl[5] >= refl[3]) {        // todo: what is refl620, refl620thresh ?
+            return 1.0f;
+        } else {
+            return UNCERTAINTY_VALUE;
+        }
+    }
+
+    public float radiometricWaterValue() {
+        if (refl[5] < refl[3]) {        // todo: what is refl620, refl620thresh ?
+            return 1.0f;
+        } else {
+            return UNCERTAINTY_VALUE;
+        }
+    }
+
+    public float getBrightWhiteThreshold() {
+        return BRIGHTWHITE_THRESH;
+    }
+
+    public float glintRiskValue() {
+        return UNCERTAINTY_VALUE;
+    }
+
+}
